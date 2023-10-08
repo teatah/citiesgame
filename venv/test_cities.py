eb914b2075649b34cadd7bd5c94533d694041aff
teata <denizag41@gmail.com>
2023-10-08 15:40:21 +0300

    Test blank input on first move

--- venv/test_cities.py
+++ venv/test_cities.py
@@ -13,6 +13,18 @@ class TestCityGame(unittest.TestCase):
         self.assertEqual("", "")
         self.assertEqual(result_label, "База городов пуста!")
 
+    def test_check_city_blank_inp(self):
+        cities = ["Москва", "Астана", "Рим"]
+        result_label = ""
+        city_label = ""
+        # Если пустой ввод, то вывести "Введите город!"
+        current_city = ""
+        new_current_city, result_label, city_label = CityGame.check_city("", current_city, cities, result_label, city_label)
+        self.assertEqual("", "")
+        self.assertEqual(result_label, "Введите город!")
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
